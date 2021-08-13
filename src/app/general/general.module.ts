a955e6df5afbc48b0d0898e627b3f1d58253ef38
sasmitha <santhiya.duskcoder@gmail.com>
2021-08-13 16:09:17 +0530

    login register genral design changed by sasmitha

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-
+import{RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -14,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
     FormsModule,
     ReactiveFormsModule,
     IonicModule,
+    RouterModule,
   ],
   declarations: [
     LoginComponent,
