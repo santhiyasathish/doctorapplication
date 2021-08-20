99ab9e4e3067d260b545b0cb778ad70110bd4493
unknown <santhiya.duskcoder@gmail.com>
2021-08-20 19:40:23 +0530

    doctor dashboard completed by jana

--- src/app/doctor/home/home.component.ts
+++ src/app/doctor/home/home.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { DoctorserviceService } from '../doctorservice.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  providers: [DatePipe],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  
+  dates=[
+    {
+      date:'20-08-2021',
+      time:'10:20 AM'
+    }
+  ];
+ 
 
-  ngOnInit() {}
+constructor(private service:DoctorserviceService, public datePipe: DatePipe) {
 
+  }
+  ngOnInit() {
+
+  }
+ 
+ 
 }
